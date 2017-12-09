3dda49ef7ea1160beead6efbc9537c35a0208af2
Markus <mamoe87@gmail.com>
2017-12-09 22:51:05 +0100

    made useable

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 $(document).ready(() => {
-  $('#searchForm').on('submit', (e) => {
-    let searchText = $('#searchText').val();
-    getMovies(searchText);
+  $("#searchForm").on('submit', (e) => {
     e.preventDefault();
+    let searchText = $("#searchText").val();
+    getMovies(searchText);
   });
 });
 
 function getMovies(searchText){
-  axios.get('http://www.omdbapi.com?s='+searchText)
-    .then((response) => {
-      console.log(response);
-      let movies = response.data.Search;
+  //make request to api using axios
+  // Make a request for a user with a given ID
+  axios.get("https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchText)
+    .then(function (response) {
+      let movies = response.data.results;
       let output = '';
       $.each(movies, (index, movie) => {
-        output += `
+        output+=`
           <div class="col-md-3">
             <div class="well text-center">
-              <img src="${movie.Poster}">
-              <h5>${movie.Title}</h5>
-              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+              <h5>${movie.title}</h5>
+              <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
             </div>
           </div>
         `;
       });
-
       $('#movies').html(output);
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(function (error) {
+      console.log(error);
     });
 }
 
@@ -39,44 +39,40 @@ function movieSelected(id){
 
 function getMovie(){
   let movieId = sessionStorage.getItem('movieId');
-
-  axios.get('http://www.omdbapi.com?i='+movieId)
-    .then((response) => {
-      console.log(response);
-      let movie = response.data;
-
-      let output =`
+  // Make a request for a user with a given ID
+  axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817")
+    .then(function (response) {
+    let movie = response.data;
+    //console.log(movie);
+    let output = `
         <div class="row">
           <div class="col-md-4">
-            <img src="${movie.Poster}" class="thumbnail">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
           </div>
           <div class="col-md-8">
-            <h2>${movie.Title}</h2>
+            <h2>${movie.title}</h2>
             <ul class="list-group">
-              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}, ${movie.genres[1].name}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
+              <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} min.</li>
+              <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name} min.</li>
             </ul>
           </div>
         </div>
         <div class="row">
           <div class="well">
             <h3>Plot</h3>
-            ${movie.Plot}
+            ${movie.overview}
             <hr>
-            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
             <a href="index.html" class="btn btn-default">Go Back To Search</a>
           </div>
         </div>
-      `;
-
-      $('#movie').html(output);
+    `;
+    $('#movie').html(output);
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(function (error) {
+      console.log(error);
     });
 }
