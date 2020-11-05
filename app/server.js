e48bcfdd658c7e3095126df768ee756d6ccc3c86
Rajesh <rajeshkumarveer2305@gmail.com>
2020-11-05 15:05:37 +0530

    Add new HTTP methods and also update movie.html

--- app/server.js
+++ app/server.js
@@ -21,19 +21,17 @@ app.post('/movies', async(req,res)=>{
 })
 
 app.put('/movies/:id',async(req,res)=>{
-    const id=req.params;
-    const olddata = await getMovieWithId(id.id);
-    console.log(olddata);
+    const olddata = await getMovieWithId(req.params.id);
     const newdata=req.body;
+    console.log(olddata);
     console.log(newdata);
     const movie = await putMovies({olddata,newdata});
     res.end();
 })
 app.delete('/movies/:id',async(req,res)=>{
-    const id=req.params;
-    console.log(id);
-    const toDeletedata = await getMovieWithId(id.id);
-    console.log(toDeletedata);
+    //console.log(req.param);
+    const toDeletedata = await getMovieWithId(req.param.id);
+    //console.log(toDeletedata);
     const Deleteddata = await deleteMovies(toDeletedata);
     res.end();
 })
